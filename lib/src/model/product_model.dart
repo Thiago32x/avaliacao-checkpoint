190c3f46afad72f24ef42960b534d9208d72274d
Thiago32x <redx32909@gmail.com>
2026-05-26 09:40:43 -0300

    feat:criados os modelos product e rating

---
+++ lib/src/model/product_model.dart
@@ -0,0 +1,45 @@
+import 'rating_model.dart';
+
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final Rating rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'].toDouble(),
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+      rating: Rating.fromJson(json['rating']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'category': category,
+      'image': image,
+      'rating': rating.toJson(),
+    };
+  }
+}
